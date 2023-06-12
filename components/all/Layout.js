dab80b57be846f80ac92f5e786499f1dade60883
ydk1204 <ejrbdi@naver.com>
2023-06-12 22:09:01 +0900

    모바일, 태블릿 플랫폼 디자인

--- components/all/Layout.js
+++ components/all/Layout.js
@@ -6,6 +6,8 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
+        <script src="//cdn.jsdelivr.net/npm/eruda"></script>
+        <script>eruda.init();</script>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@400;700&family=Pacifico&family=Roboto:wght@100;300;400&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet"></link>
--- pages/movies/movieDetail/[...params].js
+++ pages/movies/movieDetail/[...params].js
@@ -32,23 +32,25 @@ export default function Detail({params}) {
   // 비슷한 영화 찾기 => 여기서 쓰는 movieNumber는 tmdb에서 사용되는 api를 쓰기 위해서 tmdb의 영화 넘버를 따로 찾아서 써야댐
   // 위의 movieNumber는 kobic에서 쓰이는 넘버임
   const similarMovie = (movieNumber) => {
-    fetch("http://localhost:3000/data/tmdb_movie_similar.json")
+    fetch("http://192.168.0.21:3000/data/tmdb_movie_similar.json")
+    // fetch("http://localhost:3000/data/tmdb_movie_similar.json") 
       .then((result) => result.json())
       .then((res) => {
         const { results } = res;
         setSimilar((prev) => [...prev, results]);
       })
-      .catch((error) => console.log("error", error));
+      .catch((error) => console.log("비슷한 영화 error", error));
   }
 
   useEffect(() => {
-    movieNumber && fetch("http://localhost:3000/data/kobic_detailMovie.json")
+    movieNumber && fetch("http://192.168.0.21:3000/data/kobic_detailMovie.json")
+    // movieNumber && fetch("http://localhost:3000/data/kobic_detailMovie.json")
     .then((result) => result.json())
       .then((res) => {
         const { movieInfoResult } = res; 
         setDetailData((prev) => [...prev, movieInfoResult.movieInfo])
     })
-      .catch((error) => console.log("error", error));
+      .catch((error) => console.log("상세 정보 error", error));
     movieGenreList.length == 0 && genreCheck();
   }, [movieNumber])
 
@@ -59,13 +61,14 @@ export default function Detail({params}) {
   // 영화 예고편
   useEffect(() => {
     // 예고편이 없는 경우 대비해야 댐
-    objData.id && fetch("http://localhost:3000/data/movie_video.json")
+    objData.id && fetch("http://192.168.0.21:3000/data/movie_video.json")
+    // objData.id && fetch("http://localhost:3000/data/movie_video.json")
       .then((result) => result.json())
       .then((res) => {
         const { results } = res;
         setVideoKey((prev) => results[0].key);
       })
-      .catch((error) => console.log("error", error));
+      .catch((error) => console.log("예고편 error", error));
   }, [objData]);
 
   
@@ -74,8 +77,8 @@ export default function Detail({params}) {
       <Seo title={title} />
       {objData ? 
         <div className={`
-          w-full h-[150rem] pt-64 text-white
-          before:content-[""] before:w-full before:h-[150rem]
+          w-full h-[170rem] xl:h-[150rem] md:h-[165rem] pt-64 text-white
+          before:content-[""] before:w-full before:xl:h-[150rem] before:md:h-[165rem] before:h-[170rem]
           before:bg-gradient-to-t before:from-[#0A1B2A] before:via-black/70  before:to-[#0A1B2A]
           before:top-0 before:left-0
           before:absolute
@@ -88,23 +91,23 @@ export default function Detail({params}) {
           }}
         >
         <div className='relative h-full flex flex-col items-center'>
-          <div className='w-[70rem] flex justify-center'>
-              <div className='h-fit w-fit border-2 border-rose-500 rounded-md overflow-hidden mr-[4rem]'>
+          <div className='xl:w-[70rem] w-full flex flex-col xl:flex-row md:flex-col items-center md:items-center xl:items-start xl:justify-center text-center xl:text-start'>
+              <div className='h-full xl:h-fit w-fit border-2 border-rose-500 rounded-md overflow-hidden xl:mr-[4rem] mb-8 xl:mb-0'>
                 <img src={`https://image.tmdb.org/t/p/w${300}${objData.poster_path}`} alt="poster" />
               </div>
-              <div className='w-[40rem] h-full leading-relaxed'>
+              <div className='xl:w-[40rem] w-full h-full leading-relaxed'>
                 <h1 className='text-4xl font-medium'>{name}</h1>
                 <h3 className='text-xl text-gray-400 mb-4'>{objData.original_title}</h3>
                 {detailData.length > 0 ? 
                   <div className='h-full'>
                     <h3 className='text-xl border-b'>감독</h3>
-                    <p className='h-[2rem] bg-black/50 flex items-center mb-2'>{detailData[0].directors[0].peopleNm}</p>
+                    <p className='h-[2rem] bg-black/50 flex items-center justify-center xl:justify-start mb-2'>{detailData[0].directors[0].peopleNm}</p>
                     <h3 className='text-xl border-b'>개봉년도</h3>  
-                    <p className='h-[2rem] bg-black/50 flex items-center mb-2'>{detailData[0].openDt}</p>
+                    <p className='h-[2rem] bg-black/50 flex items-center justify-center xl:justify-start mb-2'>{detailData[0].openDt}</p>
                     <h3 className='text-xl border-b'>상영시간</h3>
-                    <p className='h-[2rem] bg-black/50 flex items-center mb-2'>{detailData[0].showTm}분</p>
+                    <p className='h-[2rem] bg-black/50 flex items-center justify-center xl:justify-start mb-2'>{detailData[0].showTm}분</p>
                     <h3 className='text-xl border-b'>장르</h3>
-                    <p className='h-[2rem] bg-black/50 flex items-center mb-2'>장르 : {
+                    <p className='h-[2rem] bg-black/50 flex items-center justify-center xl:justify-start mb-2'>장르 : {
                         movieGenreList.map((gen, idx) => (
                           <span className='mr-1' key={idx}>{gen}{
                             idx !== movieGenreList.length - 1 ? ',' : ''
@@ -112,7 +115,7 @@ export default function Detail({params}) {
                       ))
                     }</p>
                     <h3 className='text-xl border-b'>상영 타입</h3>
-                      <div className='grid gap-3 overflow-scroll 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-2 place-items-center bg-black/50 h-[15rem] rounded-md mb-2'>
+                      <div className='grid gap-3 overflow-auto 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-2 place-items-center bg-black/50 h-[15rem] rounded-md mb-2'>
                       {
                         detailData[0].showTypes.map((type, idx) => (
                           <div className='w-[8rem] h-[6rem] bg-white/90 flex flex-col items-center justify-center rounded-lg text-black' key={idx}>
@@ -122,7 +125,7 @@ export default function Detail({params}) {
                       ))}
                       </div>
                     <h3 className='text-xl border-b'>개요</h3>
-                    <p className='h-[15rem] px-3 overflow-scroll bg-black/50 flex items-center mb-2'>{objData.overview}</p>
+                    <p className='h-[15rem] px-3 overflow-auto bg-black/50 flex items-center mb-2'>{objData.overview}</p>
                     <h3 className='mb-1 text-xl border-b'>예고편</h3>
                     <div className='h-[25rem] mb-2'>
                       <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${videoKey}?autoplay=0`}></iframe>  
@@ -133,8 +136,8 @@ export default function Detail({params}) {
                     }
                   </div>
             </div>
-            <h3 className='mt-20 text-3xl border-b w-[63rem] text-center'>비슷한 영화</h3>
-            <div className='w-[63rem] h-[30rem] bg-black/50 flex items-center rounded-md mb-10'>
+            <h3 className='mt-20 text-3xl border-b xl:w-[63rem] w-full text-center'>비슷한 영화</h3>
+            <div className='xl:w-[63rem] w-full h-[30rem] bg-black/50 flex items-center rounded-md mb-10'>
               <div className='container relative '>
                 <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} onWheel={onWheel}>
                 {similar && similar[0]?.map((movie, idx) => (
